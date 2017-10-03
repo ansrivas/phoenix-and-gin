944284108fa59aace16361d724f64a0a2d8b4f39
Ankur Srivastava <ankur.srivastava@recogizer.de>
2017-10-03 02:00:45 +0200

    first version of phoenix app

---
+++ phoenix_app/benchmarker/web/router.ex
@@ -0,0 +1,24 @@
+defmodule Benchmarker.Router do
+  use Benchmarker.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Benchmarker do
+    pipe_through :browser # Use the default browser stack
+
+    get "/:title", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", Benchmarker do
+  #   pipe_through :api
+  # end
+end
